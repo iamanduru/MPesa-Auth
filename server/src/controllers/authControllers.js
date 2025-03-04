370452de4643d634cc88157c49d719770a53fe1b
Mitchelle <104675426+iamanduru@users.noreply.github.com>
2025-03-04 23:14:18 +0200

    Authentication routes

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -47,7 +47,7 @@ exports.login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(401).json({ message: "Invalid Password!" });
 
-        //Genrate JWT token
+        //Generate JWT token
         const token = generateToken(user);
 
         res.status(200).json({message: "Login Successful", token });
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,1 +1,10 @@
 //Authentication routes
+const express = require("express");
+const { signup, login } = require("../controllers/authControllers");
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+
+module.exports = router;
