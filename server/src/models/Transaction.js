7270b58d2b51d23ef5cd17069263634c0385f117
Mitchelle <104675426+iamanduru@users.noreply.github.com>
2025-03-04 22:02:06 +0200

    MongoDB Schemas done!

--- server/src/models/Transaction.js
+++ server/src/models/Transaction.js
@@ -1,1 +1,15 @@
 //Payment transaction schema
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema(
+    {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        mpesaRecieptNumber: { type: String, unique: true, required: true },
+        amount: { type: Number, required: true},
+        status: { type: String, enum: ["pending", "success", "failed"], default: "pending" },
+        transactionDate: { type: Date, default: Date.now },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
